1a14a2bc8f9aa35bfd4d7eae88344002ef11bff3
Samuel-Brandt <samuelbrandt007@gmail.com>
2026-03-24 17:45:26 -0300

    Add files via upload

---
+++ Funcoes.java
@@ -0,0 +1,410 @@
+import java.util.Scanner;
+public class Funcoes
+{
+        Scanner sc = new Scanner(System.in);
+        // Parte 1
+        void VelocidadeMedia() {
+            System.out.print("\nQual a posição inicial(Si): ");
+                int Si=sc.nextInt();
+            System.out.print("Qual a posição final(S): ");
+                int S=sc.nextInt();
+            System.out.print("Qual o tempo inicial(Ti): ");
+                int Ti=sc.nextInt();
+            System.out.print("Qual o tempo Final(T): ");
+                int T=sc.nextInt();
+                float V = (S-Si)/(T-Ti);
+            System.out.println("\u001B[34mA velocidade média é: "+ V+"Km/h");
+        }
+        // Parte 2
+        void MovimentoUniforme() {
+            System.out.println("\n\u001B[35mOpções\u001B[0m");
+            System.out.println("[1] Calcular a posição [S = Si+(V*T)]");
+            System.out.println("[2] Calcular o tempo [T = (S - Si)/V]");
+            System.out.print("\u001B[36mEscolha: \u001B[0m");
+                int mov=sc.nextInt();
+                
+            switch (mov) {
+            case 1:
+                System.out.print("\nQual a posição inicial(Si): ");
+                    int Si=sc.nextInt();
+                System.out.print("Qual a velocidade media(V): ");
+                    int V=sc.nextInt();
+                System.out.print("Qual o tempo final(T): ");
+                    int T=sc.nextInt();
+                    int S = Si+(V*T);
+                System.out.println("\u001B[34mA sua posição é: "+S+"m");
+            break;
+                case 2:
+                    System.out.print("Qual a posição inicial(Si): ");
+                        Si=sc.nextInt();
+                    System.out.print("\nQual a posição final(S): ");
+                        S=sc.nextInt();
+                    System.out.print("Qual a velocidade media(V): ");
+                        V=sc.nextInt();
+                        T = (S - Si)/V;
+                    System.out.println("\u001B[34mO tempo é: "+T+"s");
+                break;
+            default: 
+                System.out.println("Erro. Escolha alguma opção certa");
+            break;
+            }
+        }
+        // Parte 3
+        void AceleracaoMedia() {
+             System.out.print("\nQual a velocidade inicial(Vi): ");
+                int Vi=sc.nextInt();
+             System.out.print("Qual a velocidade final(V): ");
+                int V=sc.nextInt();
+             System.out.print("Qual o tempo inicial(Ti): ");
+                int Ti=sc.nextInt();
+             System.out.print("Qual o tempo final(T): ");
+                int T=sc.nextInt();
+                int a = (V-Vi)/(T-Ti);
+             System.out.println("\u001B[34mA aceleração é: "+a+"m/s");
+        }
+        // Parte 4
+        void FunçãoHorariaDaVelocidade() {
+            System.out.print("\nQual a velocidade inicial(Vi): ");
+                int Vi=sc.nextInt();
+            System.out.print("Qual a aceleração(a): ");
+                int a=sc.nextInt();
+            System.out.print("Qual o tempo(T): ");
+                int T=sc.nextInt();
+                int V = Vi+(a*T);
+            System.out.println("\u001B[34mA velocidade é: "+V+"m/s");
+    }
+        // Parte 5
+        void FunçãoHorariaDasPosições() {
+            System.out.print("\nQual a posição inicial(Si): ");
+                int Si=sc.nextInt();
+            System.out.print("Qual a velocidade inicial(Vi): ");
+                int Vi=sc.nextInt();
+            System.out.print("Qual o tempo(T): ");
+                int T=sc.nextInt();
+            System.out.print("Qual a aceleração(a): ");
+                int a=sc.nextInt();
+                int S = (Si+(Vi*T))+((a*(T*T))/2);
+            System.out.println("\u001B[34mA sua posição é :"+S+"m");
+        }
+        // Parte 6
+        // falta fazer o metodo da exaustão, para ficar com 
+        // a resposta completa.
+        void EquaçãoDeTorricelli() {
+            System.out.print("\nQual é a velocidade inicial(Vi): ");
+                int Vi=sc.nextInt();
+            System.out.print("Qual a aceleração(a): ");
+                int a=sc.nextInt();
+            System.out.print("Qual a posição(ΔS): ");
+                int ΔS=sc.nextInt();
+                Vi = Vi*Vi;
+                int V = Vi+(2*a*ΔS);
+            System.out.println("\u001B[34mA raíz quadrada vai ficar: √"+V);
+        }
+        // Parte 7
+        void LVC() {
+            System.out.println("/n\u001B[35mOpções\u001B[0m");
+            System.out.println("[1] [H = Hi-(Vi*T)-(g*T²/2)]");
+            System.out.println("[2] [V = -Vi-g*T]");
+            System.out.println("[3] [V² = Vi²+2*g*ΔH]");
+            System.out.print("\u001B[36mEscolha: \u001B[0m");
+                int esc=sc.nextInt();
+            
+            switch (esc) {
+                case 1:
+                    System.out.print("\nQual a altura incial(Hi): ");
+                        int Hi=sc.nextInt();
+                    System.out.print("Qual a velocidade inicial(Vi): ");
+                        int Vi=sc.nextInt();
+                    System.out.print("Qual o tempo(T): ");
+                        int T=sc.nextInt();
+                    System.out.print("Qual a gravidade(g): ");
+                        float g=sc.nextInt();
+                        float H = (Hi+(Vi*T))-((g*(T*T))/2);
+                    System.out.print("\u001B[34mA altura é: "+H+"m");
+                break;
+                    case 2:
+                System.out.print("\nQual a velocidade inicial(Vi): ");
+                        Vi=sc.nextInt();
+                    System.out.print("Qual a gravidade(g): ");
+                        g=sc.nextInt();
+                    System.out.print("Qual o tempo(T): ");
+                        T=sc.nextInt();
+                        float V = -Vi-(g*T);
+                    System.out.print("\u001B[34mA velocidade é: "+V+"s");
+                    break;
+                case 3:
+                    System.out.print("\nQual a velocidade inicial(Vi): ");
+                        Vi=sc.nextInt();
+                    System.out.print("Qual a gravidade(g): ");
+                        g=sc.nextInt();
+                    System.out.print("Qual o delta da altura(ΔH): ");
+                        int ΔH=sc.nextInt();
+                        V = (Vi*Vi)+(2*g*ΔH);
+                    System.out.print("\u001B[34mA raíz quadrada vai ficar: √"+V);
+                break;
+                    default: 
+                        System.out.println("Erro. Escolha alguma opção certa");
+                    break;
+            }
+        }
+        // Parte 8
+        void Q() {
+            System.out.println("/n\u001B[35mOpções\u001B[0m");
+            System.out.println("[1] [H = Hi-(Vi*T)-(g*T²/2)]");
+            System.out.println("[2] [V = -Vi-g*T]");
+            System.out.println("[3] [V² = 0-2*g*ΔH]");
+            System.out.print("\u001B[36mEscolha: \u001B[0m");
+                int esc=sc.nextInt();
+            
+            switch (esc) {
+                case 1:
+                    System.out.print("\nQual a altura incial(Hi): ");
+                        int Hi=sc.nextInt();
+                    System.out.print("Qual a velocidade inicial(Vi): ");
+                        int Vi=sc.nextInt();
+                    System.out.print("Qual o tempo(T): ");
+                        int T=sc.nextInt();
+                    System.out.print("Qual a gravidade(g): ");
+                        float g=sc.nextInt();
+                        float H = (Hi-(Vi*T))-((g*(T*T))/2);
+                    System.out.print("\u001B[34mA altura é: "+H+"m");
+                    
+                break;
+                    case 2:
+                System.out.print("\nQual a velocidade inicial(Vi): ");
+                        Vi=sc.nextInt();
+                    System.out.print("Qual a gravidade(g): ");
+                        g=sc.nextInt();
+                    System.out.print("Qual o tempo(T): ");
+                        T=sc.nextInt();
+                        float V = -Vi-(g*T);
+                    System.out.print("\u001B[34mA velocidade é: "+V+"s");
+                    break;
+                case 3:
+                    System.out.print("\nQual a velocidade inicial(Vi): ");
+                        Vi=sc.nextInt();
+                    System.out.print("Qual a gravidade(g): ");
+                        g=sc.nextInt();
+                    System.out.print("Qual o delta da altura(ΔH): ");
+                        int ΔH=sc.nextInt();
+                        V = (-Vi*Vi)-(2*g*ΔH);
+                    System.out.print("\u001B[34mA raíz quadrada vai ficar: √"+V);
+                break;
+                    default: 
+                        System.out.println("Erro. Escolha alguma opção certa");
+                    break;
+        }
+    }
+        // parte 9
+        void Ql() {
+            System.out.println("\n\u001B[35mOpções\u001B[0m");
+            System.out.println("[1] [H = Hi-(0*T)-(g*T²/2)]");
+            System.out.println("[2] [V = 0-g*T]");
+            System.out.println("[3] [V² = -Vi²+2*g*ΔH]");
+            System.out.print("\u001B[36mEscolha: \u001B[0m");
+                int esc=sc.nextInt();
+            
+            switch (esc) {
+                case 1:
+                    System.out.print("\nQual a altura incial(Hi): ");
+                        int Hi=sc.nextInt();
+                    System.out.print("Qual o tempo(T): ");
+                        int T=sc.nextInt();
+                    System.out.print("Qual a gravidade(g): ");
+                        float g=sc.nextInt();
+                        float H = (Hi-(0*T))-((g*(T*T))/2);
+                    System.out.print("\u001B[34mA altura é: "+H+"m");
+                    
+                break;
+                    case 2:
+                        System.out.print("\nQual a gravidade(g): ");
+                            g=sc.nextInt();
+                        System.out.print("Qual o tempo(): ");
+                            T=sc.nextInt();
+                            float V = 0-(g*T);
+                        System.out.print("\u001B[34mA velocidade é: "+V+"s");
+                        break;
+                case 3:
+                    System.out.print("\nQual a gravidade(g): ");
+                        g=sc.nextInt();
+                    System.out.print("Qual o delta da altura(ΔH): ");
+                        int ΔH=sc.nextInt();
+                        V = 0-(2*g*ΔH);
+                    System.out.println("\u001B[34mA raíz quadrada vai ficar: √"+V);
+                break;
+                    default: 
+                        System.out.println("Erro. Escolha alguma opção certa");
+                    break;
+            }
+        }
+        // Parte 10
+        void VelocidadeAngular() {
+            System.out.println("\n\u001B[35mOpções\u001B[0m");
+            System.out.println("[1] [W = Δθ/ΔT]");
+            System.out.println("[2] [W = 2π/t]");
+            System.out.print("\u001B[36mEscolha: \u001B[0m");
+                int esc=sc.nextInt();
+                
+            switch (esc) {
+                case 1:
+                    System.out.print("\nQual o agulo inicial(θ1): ");
+                        int θi=sc.nextInt();
+                    System.out.print("Qual o angulo final(θ2): ");
+                        int θ=sc.nextInt();
+                    System.out.print("Qual o tempo inicial(Ti): ");
+                        int Ti=sc.nextInt();
+                    System.out.print("Qual o tempo final(T): ");
+                        int T=sc.nextInt();
+                        int W = (θ-θi)/(T-Ti);
+                        W = 180/W;
+                    System.out.println("\u001B[34mA velocidade angular é: "+W+"π rad/s");
+                break;
+                    case 2:
+                        System.out.print("\nQual a periodo(T): ");
+                            T=sc.nextInt();
+                            W = 2*T;
+                        System.out.print("\u001B[34mA velocidade angular é: "+W+"π rad/s");
+                    break;
+                        default: 
+                            System.out.println("Erro. Escolha alguma opção certa");
+                        break;
+            }
+        }
+        // Parte 11
+        void VelocidadeEscalar() {
+            System.out.print("\nQual o raio(R): ");
+                int R=sc.nextInt();
+            System.out.print("Qual o periodo(T): ");
+                int T=sc.nextInt();
+                int V = (2*R)/T;
+            System.out.println("\u001B[34mA velocidade escalar é: "+V+"π m/s");
+        }
+        // Parte 12
+        void Pesos() {
+            System.out.print("\n\u001B[35mQual seria o peso de alguma massa em todo o sistema solar\u001B[0m");
+            System.out.println("[1] Terra");
+            System.out.println("[2] Lua");
+            System.out.println("[3] Mercúrio");
+            System.out.println("[4] Vênus");
+            System.out.println("[5] Marte");
+            System.out.println("[6] Júpiter");
+            System.out.println("[7] Saturno");
+            System.out.println("[8] Urano");
+            System.out.println("[9] Netuno");
+            System.out.print("\u001B[36mEscolha: \u001B[0m");
+                int esc=sc.nextInt();
+                
+                switch (esc) {
+                    case 1:
+                        System.out.print("Quantos kilos pesa: ");
+                            int kg=sc.nextInt();
+                            double val = kg*9.81;
+                        System.out.println("\u001B[34mO seu peso na Terra é: "+val);
+                    break;
+                        case 2:
+                            System.out.print("Quantos kilos pesa: ");
+                                kg=sc.nextInt();
+                                val = kg*1.6;
+                            System.out.println("\u001B[34mO seu peso na Lua é: "+val);   
+                        break;
+                    case 3:
+                        System.out.print("Quantos kilos pesa: ");
+                            kg=sc.nextInt();
+                            val = kg*3.7;
+                        System.out.println("\u001B[34mO seu peso na Mercúrio é: "+val);
+                    break;
+                        case 4:
+                            System.out.print("Quantos kilos pesa: ");
+                            kg=sc.nextInt();
+                            val = kg*8.87;
+                        System.out.println("\u001B[34mO seu peso na Vênus é: "+val);
+                        break;
+                    case 5:
+                        System.out.print("Quantos kilos pesa: ");
+                            kg=sc.nextInt();
+                            val = kg*3.71;
+                        System.out.println("\u001B[34mO seu peso na Marte é: "+val);
+                    break;
+                        case 6:
+                            System.out.print("Quantos kilos pesa: ");
+                            kg=sc.nextInt();
+                            val = kg*24.79;
+                        System.out.println("\u001B[34mO seu peso na Júpiter é: "+val);
+                        break;
+                    case 7:
+                        System.out.print("Quantos kilos pesa: ");
+                            kg=sc.nextInt();
+                            val = kg*10.44;
+                        System.out.println("\u001B[34mO seu peso na Saturno é: "+val);
+                    break;
+                        case 8:
+                            System.out.print("Quantos kilos pesa: ");
+                            kg=sc.nextInt();
+                            val = kg*8.69;
+                        System.out.println("\u001B[34mO seu peso na Urano é: "+val);
+                        break;
+                    case 9:
+                        System.out.print("Quantos kilos pesa: ");
+                            kg=sc.nextInt();
+                            val = kg*11.15;
+                        System.out.println("\u001B[34mO seu peso na Netuno é: "+val);
+                    break;
+                        default: 
+                            System.out.println("Erro. Escolha alguma opção certa");
+                        break;
+                }
+            
+        }
+    // Parte 13
+    void EscalaTermica() {
+         System.out.print("\n\u001B[35mQual temperatura deseja calcular?\u001B[0m");
+         System.out.println("");
+         System.out.println("\n[1] \u001B[32mCelsius (°C)\u001B[0m para \u001B[34mFahrenheit (°F)\u001B[0m");
+         System.out.println("[2] \u001B[34mFahrenheit (°F)\u001B[0m para \u001B[32mCelsius (°C)\u001B[0m");
+         System.out.println("[3] Kelvin (°K) para \u001B[32mCelsius (°C)\u001B[0m");
+         System.out.println("[4] \u001B[32mCelsius (°C)\u001B[0m para Kelvin (°K)");
+         System.out.println("[5] Kelvin (°K) para \u001B[34mFahrenheit (°F)\u001B[0m");
+         System.out.println("[6] \u001B[34mFahrenheit (°F)\u001B[0m para Kelvin (°K)");
+         System.out.print("\u001B[36mEscolha: \u001B[0m");
+         int esc=sc.nextInt();
+         
+         switch (esc){
+             case 1:
+                 System.out.println("Digite quantos graus em °C");
+                 float Tc=sc.nextInt();
+                 float total= Tc/5;
+                 total = (total*9)+ 32;
+                 System.out.println("\u001B[34mFicou "+total+"°F");
+             break;
+                case 2:
+                     System.out.println("Digite quantos graus em °F");
+                     float Tf=sc.nextInt();
+                     total = (Tf- 32)/9;
+                     total = total*5;
+                     System.out.println("\u001B[34mFicou "+total+"°C");
+                break;
+             case 3:
+                 System.out.println("Digite quantos graus em °K");
+                 float Tk=sc.nextInt();
+                 total = Tk- 273;
+                 System.out.println("\u001B[34mFicou "+total+"°C");
+             break;
+                case 4:
+                     
+                break;
+            case 5:
+                 System.out.println("Digite quantos graus em °K");
+                 Tk=sc.nextInt();
+                 total = (Tk- 273)/5;
+                 total = (total*9)+ 32;
+                 System.out.println("\u001B[34mFicou "+total+"°F");
+             break;
+                case 6:
+                     
+                break;
+                default: 
+                    System.out.println("Erro. Escolha alguma opção certa");
+                break;
+         }
+    }
+}
---
+++ Main.java
@@ -0,0 +1,71 @@
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		Funcoes ps = new Funcoes();
+		
+		System.out.println("\u001B[35mEscolha alguma opção a seguir\u001B[0m");
+		System.out.println("\n[1] Velocidade Média \u001B[33m[V = ΔS/ΔT]\u001B[0m");
+		System.out.println("[2] Movimento Uniforme \u001B[33m[S = Si+(V*T)] | [T = (S - Si)/V]\u001B[0m");
+		System.out.println("[3] Aceleração Média \u001B[33m[a = ΔV/ΔT]\u001B[0m");
+		System.out.println("[4] Função Horaria Da Velocidade \u001B[33m[V = Vi+(a*T)]\u001B[0m");
+		System.out.println("[5] Função Horaria Das Posições \u001B[33m[S = (Si+(Vi*T))+((a*T²)/2)]\u001B[0m");
+		System.out.println("[6] Equaçao de Torricelli \u001B[33m[V² = Vi²+2*a*ΔS]\u001B[0m");
+		System.out.println("[7] LVC \u001B[33m[H = Hi+(Vi*T)-(g*T²/2)] | [V = Vi-g*T] | [V² = Vi²+2*g*ΔH]\u001B[0m");
+		System.out.println("[8] Q \u001B[33m[H = Hi-(Vi*T)-(g*T²/2)] | [V = -Vi-g*T] | [V² = -Vi²-2*g*ΔH]\u001B[0m");
+		System.out.println("[9] QL \u001B[33m[H = Hi-(0*T)-(g*T²/2)] | [V = 0-g*T] | [V² = 0-2*g*ΔH]\u001B[0m");
+		System.out.println("[10] Velocidade angular \u001B[33m[W = Δθ/ΔT] | [W = 2π/t]\u001B[0m");
+		System.out.println("[11] Velocidade escalar \u001B[33m[V = 2π*R/t]\u001B[0m");
+		System.out.println("[12] Peso de um corpo \u001B[33m[P = m*a]\u001B[0m");
+		System.out.println("[13] Escala Termica \u001B[33m[Tc/5 = Tf-32/9] | [Tc = Tk-273] | [Tf-32/9 = Tk-273/5]\u001B[0m");
+		System.out.print("\u001B[36mEscolha: \u001B[0m");
+            int opcao=sc.nextInt();
+         
+		switch (opcao) {
+            case 1:
+                ps.VelocidadeMedia();
+            break;
+                case 2:
+                    ps.MovimentoUniforme();
+                break;
+            case 3:
+               ps.AceleracaoMedia();
+            break;
+                case 4:
+                    ps.FunçãoHorariaDaVelocidade();
+                break;
+            case 5:
+                ps.FunçãoHorariaDasPosições();
+            break;
+                case 6:
+                    ps.EquaçãoDeTorricelli();
+                break;
+            case 7:
+                ps.LVC();
+            break;
+                case 8:
+                  ps.Q();  
+                break;
+            case 9:
+                ps.Ql();
+            break;
+                case 10:
+                    ps.VelocidadeAngular();
+                break;
+            case 11:
+                ps.VelocidadeEscalar();
+            break;
+                case 12:
+                    ps.Pesos();
+                break;
+            case 13:
+                ps.EscalaTermica();
+            break;
+                default:
+                    System.out.println("Erro. Digite um número válido");
+                break;
+		}
+		
+	}
+}
